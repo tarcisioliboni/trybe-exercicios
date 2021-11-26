e800288efe25c39329f8726753d1f35e2fd7ccd6
Tarcisio Liboni <tarcisioliboni@gmail.com>
2021-11-26 18:38:19 -0300

    Adicionado 5 programas de operações matemáticas

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa1.js
@@ -0,0 +1,17 @@
+let a = 10;
+let b =5;
+
+let sum = a + b;
+console.log(sum)
+
+let subt = a -b;
+console.log (subt);
+
+let mult = a * b;
+console.log (mult);
+
+let divi = a / b;
+console.log (divi);
+
+let modu = a % b;
+console.log (modu);
