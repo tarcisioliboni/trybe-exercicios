b94b99388c2e02d123489ff00146a1642f2e7764
Tarcisio Liboni <tarcisioliboni@gmail.com>
2021-11-26 18:49:08 -0300

    Maior de dois números

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/maiordedoisnumeros.js
@@ -0,0 +1,10 @@
+const a = 50;
+const b = 50;
+
+if (a > b){
+    console.log ('O primeiro número é maior');
+} else if (a < b){
+    console.log ('O segundo número é maior');
+} else {
+    console.log ('Os números são iguais');
+}
