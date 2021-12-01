8f07bbf343ce94f044dafb401ca002f8c308c39c
Tarcisio Liboni <tarcisioliboni@gmail.com>
2021-12-01 18:09:03 -0300

    Finalizada a parte 4

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parteI.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parteI.js
@@ -2,6 +2,19 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    // Parte 2
+    recorrente: 'Sim', 
   };
 
-  console.log('Bem vinda,', info['personagem']);
+  // Parte 1
+//   console.log('Bem vinda,', info['personagem']);
+
+  // Parte 3
+//   for (let header in info){
+//       console.log(header);
+//   }
+
+//   Parte 4
+    for (let header in info){
+        console.log(info[header]);
+    }
