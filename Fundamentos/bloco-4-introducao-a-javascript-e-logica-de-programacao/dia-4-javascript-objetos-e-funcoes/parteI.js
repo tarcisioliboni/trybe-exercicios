0337d467254401e8b4a783b6b68eb4eb4ef7db6e
Tarcisio Liboni <tarcisioliboni@gmail.com>
2021-12-02 10:24:30 -0300

    Finalizado o exercicio 5

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parteI.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parteI.js
@@ -5,16 +5,33 @@ let info = {
     // Parte 2
     recorrente: 'Sim', 
   };
+  
+  let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain Dells Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    // Parte 2
+    recorrente: 'Sim', 
+  };
 
-  // Parte 1
+//   Parte 1
 //   console.log('Bem vinda,', info['personagem']);
 
-  // Parte 3
+//   Parte 3
 //   for (let header in info){
 //       console.log(header);
 //   }
 
 //   Parte 4
-    for (let header in info){
-        console.log(info[header]);
+//     for (let header in info){
+//         console.log(info[header]);
+//   }
+
+// Parte 5
+    for (index in info) {
+      if (info[index] === info2[index]){
+        console.log('Ambos recorrentes');
+      } else {
+        console.log(info[index], "e", info2[index]);
+      }
     }
