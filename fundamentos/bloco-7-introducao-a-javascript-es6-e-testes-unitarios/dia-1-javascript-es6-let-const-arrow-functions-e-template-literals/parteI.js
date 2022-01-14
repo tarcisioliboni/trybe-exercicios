37ee58edc27ff12f42928ebed3e65b7af43f631f
Tarcisio Liboni <tarcisioliboni@gmail.com>
2022-01-14 10:32:42 -0300

    Modificado para arrow function e template literals

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parteI.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parteI.js
@@ -1,10 +1,14 @@
-      function testingScope(escopo) {
+      const testingScope = (escopo) => {
         if (escopo === true) {
           let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-          ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+          ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
           console.log(ifScope);
+
+
+
+
         } else {
-          let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+          const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
           console.log(elseScope);
         }
       }
