731682b4802be650f6b931176d5137e5afe4a65c
Tarcisio Liboni <tarcisioliboni@pop-os.localdomain>
2022-02-16 15:33:02 -0300

    Content ex03 OK

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/third-part/src/Content.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import App from './App';
+
+const conteudos = [
+  {
+    conteudo: 'High Order Functions',
+    bloco: 8,
+    status: 'Aprendido'
+  },
+  {
+    conteudo: 'Composicao de Componentes',
+    bloco: 11,
+    status: 'Aprendendo',
+  },
+  {
+    conteudo: 'Composicao de Estados',
+    bloco: 12,
+    status: 'Aprenderei'
+  },
+  {
+    conteudo: 'Redux',
+    bloco: 16,
+    status: 'Aprenderei'
+  },
+];
+
+class Content extends React.Component {
+  render() {
+    return (
+      <div>
+        { conteudos.map((item) => (
+          <div key={item.conteudo}>
+            <p>{`O conteúdo é: ${item.conteudo}`}</p>
+            <p>{`Status: ${item.status}`}</p>
+            <p>{`Bloco: ${item.bloco}`}</p>
+          </div>
+          ))}
+      </div>
+    )
+  }
+}
+
+export default Content;
